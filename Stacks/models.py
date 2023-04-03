2a4e4e52cbbdc2193dd603f03083641baa77b2fc
clarissehira <clarihira@gmail.com>
2023-04-03 09:29:05 +0200

    added model

--- Stacks/models.py
+++ Stacks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class DjangoModel(models.Model):
     title=models.CharField(max_length=200)
     content=models.TextField(verbose_name="content")
@@ -8,4 +9,12 @@ class DjangoModel(models.Model):
     
     def __str__(self):
         return self.title
+
+class Node(models.Model):
+    title = models.CharField(max_length=140 ,verbose_name="node")
+    content = models.TextField(verbose_name="content")
+    progress = models.BooleanField()  
+
+    def __str__ (self):
+        return self.title        
     
