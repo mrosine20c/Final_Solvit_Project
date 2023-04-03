b765f0a9f81ccd3a1a36c8e1a327f6687d97f8ac
mrosine20c <mrosine20c@gmail.com>
2023-04-03 09:42:00 +0200

    React class model

--- Stacks/models.py
+++ Stacks/models.py
@@ -16,5 +16,11 @@ class Node(models.Model):
     progress = models.BooleanField()  
 
     def __str__ (self):
-        return self.title        
+        return self.title 
+    
+class React(models.Model):
+    title= models.CharField(max_length=140 ,verbose_name="node")
+    content = models.TextField(verbose_name="content")
+    progress = models.BooleanField()
+         
     
